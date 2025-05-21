6800b4c44caaec564aa2efaa4f119e9b85706a20
Majo247 <cruzsanchezmaria600@gmail.com>
2025-05-20 22:14:50 -0600

    Objetos&Arreglos EjercicioA 20/05/25

---
+++ Practica2/EjercicioA.js
@@ -0,0 +1,12 @@
+const persona = {
+    nombre: "María José",
+    edad: 24,
+    dieccion: {
+        ciudad: "Qro",
+        pais: "MX"
+
+    }
+}
+
+const persona2 = {nombre, edad, dieccion: {ciudad, pais} } = persona;
+console.log("Me llamo " + nombre + " tengo " + edad + " años y vivo en " + ciudad + ", " + pais);
