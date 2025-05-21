3075df521904b6da41018995274ee06f97b35eaa
Majo247 <cruzsanchezmaria600@gmail.com>
2025-05-20 22:27:34 -0600

    Objetos&Arreglos EjercicioB 20/05/25

---
+++ Practica2/EjercicioB.js
@@ -0,0 +1,12 @@
+const productos = [
+    { nombre: "Laptop", precio: 1200 },
+    { nombre: "Mouse", precio: 250 },
+    { nombre: "Teclado", precio: 750 },
+    { nombre: "Monitor", precio: 3000 }
+]
+
+
+const paso1 = productos.filter(producto => producto.precio >= 1000);
+
+const nombres = paso1.map(filtrar => filtrar.nombre);
+console.log(nombres);
