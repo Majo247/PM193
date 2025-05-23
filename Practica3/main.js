2d73caede889332df9a165a14e9af6bf8455fddd
Majo247 <cruzsanchezmaria600@gmail.com>
2025-05-23 14:25:13 -0600

    Practica3 Utils_Main 23/05/25

---
+++ Practica3/main.js
@@ -0,0 +1,4 @@
+//En esta linea importamos la función restar del archivo utils.js
+import {restar} from './utils.js';
+
+console.log(restar(50, 17)); 
---
+++ Practica3/utils.js
@@ -0,0 +1,6 @@
+// La función restar toma dos números como argumentos y devuelve su resta
+export function restar (a, b) {
+    return a - b;
+}
+
+
