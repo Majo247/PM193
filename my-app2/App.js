10e0b79e5506ecbe149728c4935db8caf6d8676e
Majo247 <cruzsanchezmaria600@gmail.com>
2025-06-10 20:22:15 -0600

    Practica Componentes, Props Y States. 10/06/2025

--- my-app2/App.js
+++ my-app2/App.js
@@ -1,15 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+/*Zona 1 : Importaciones*/
 
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+
+const Texto= (props)=>{
+  const {contenido}= props
+  return (
+    <Text>{contenido}</Text>
+  );
+}
+
+/*Zona 2 : Main zona de la ejecución*/
 export default function App() {
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+    
+      <Texto contenido= "Hola"></Texto>
+      <Texto contenido= "Mundo"></Texto>
+      <Texto contenido= "React Native"></Texto>
+      <Button title="Presionar"></Button>
       <StatusBar style="auto" />
+
     </View>
   );
 }
 
+
+/*Zona 3 : Estitos*/
 const styles = StyleSheet.create({
   container: {
     flex: 1,
