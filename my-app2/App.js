b8617564d985888f3bbf4818e774b881ff370e3c
Majo247 <cruzsanchezmaria600@gmail.com>
2025-06-12 18:00:06 -0600

    Cambiar botonContenido 12/06/25

--- my-app2/App.js
+++ my-app2/App.js
@@ -7,16 +7,20 @@ import React, {useState} from 'react';//Para omenzar el useState para el estado 
 
 //componente Propio de Texto
 const Texto= ()=>{
-  const [contenido, setContenido]=useState('Hola Mundo')
+  const [contenido, setContenido]=useState('Hola Mundo')//Para cambiar el texto del boton al presionar
   const actualizaTexto=()=>{setContenido('Estado modificado')}//Para cambiar el hola mundo por otro tecto
+  
   return (
     <Text onPress={actualizaTexto}> {contenido} </Text>
+    
    
   );
 }
 
 /*Zona 2 : Main zona de la ejecución*/
 export default function App() {
+  const [botonContenido, setBotonTexto]=useState('Presionar')
+  const cambiarTextoBoton=()=>{setBotonTexto('Presionado')} 
   return (
 
     <View style={styles.container}>
@@ -25,7 +29,7 @@ export default function App() {
       <Texto ></Texto>
       <Texto ></Texto>
       <Texto ></Texto>
-      <Button title = "Presionar"></Button>
+      <Button title = {botonContenido} onPress={cambiarTextoBoton}></Button> //En esta parte se cambia el texto del boton al presionar por lo que el onPress
       <StatusBar style="auto" />
 
     </View>
